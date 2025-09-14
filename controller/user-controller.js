2e1ee0fd44ccf4e8353ae290e20d4452014ea89d
Christian <christian.darosa0106@gmail.com>
2025-09-14 10:14:34 -0300

    bora

--- controller/user-controller.js
+++ controller/user-controller.js
@@ -65,8 +65,113 @@ let usuarios = [
 ];
 
 function ListarUsuarios(req, res){
-    console.log("cheguei no controller")
+    const limit = parseInt(req.query.limit) || usuarios.length;
+    res.status(200).json(usuarios.slice(0, limit));
 
 }
+function CriarUsuario(req,res){
+    
+    const {nome, email} = req.body;
+    if(!nome || !email){
+        return res.status(400).json({mensagem: "nome e email obrigatórios"});
+    }
 
-export {ListarUsuarios}
+    const email_valido = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+    if(!email_valido.test(email)){
+        return res.status(400).json({mensagem: "email inválido!!"})
+    }
+
+    ultimoId++;
+    const novoUsuario = { id: ultimoId, nome, email };
+    usuarios.push(novoUsuario);
+    return res.status(201).json({mensagem: "criado"});
+
+    
+};
+
+function pegar1Usuario(req,res){
+    const { id } = req.params;
+    const usuario = usuarios.find(u => u.id == idnumber)
+    const idnumber = parseInt(id);
+    if(isNaN(idnumber)){
+      return res.status(400).json({mensagem: "o id precisa ser um número inteiro"})
+    }
+    if(!usuario){
+        return res.status(404).json({mensagem: "usuario não encontrado!!"});
+    }
+    res.json(usuario);
+};
+function atualizarUsuario(req,res){
+    const { id } = req.params;
+    const idnumber = parseInt(id);
+    const { nome, email} = req.body;
+    
+
+    const usuario = usuarios.find(u =>u.id == idnumber);
+    if(isNaN(idnumber)){
+      return res.status(400).json({mensagem: "o id precisa ser um número inteiro"})
+    }
+ 
+    if(!usuario){
+        return res.status(404).json({mensagem: "usuario não encontrado!!!"});
+    };
+
+
+
+
+    if(email){
+      let email_existente = usuarios.findIndex(u =>u.email === email);
+      if(email_existente !== -1){
+      return res.status(409).json({mensagem: "email já está cadastrado"})
+      }
+      usuario.email = email
+    }
+
+    if(!nome && !email){
+      return res.status(400).json({mensagem: "manda pelo menos um"})
+    }
+
+    if(nome){
+      let nome_existente = usuarios.findIndex(u =>u.nome === nome);
+      if(nome_existente !== -1){
+      return res.status(409).json({mensagem: "email já está cadastrado"})
+      }
+      usuario.nome = nome
+    }
+
+    if(email && nome){
+      let email_existente = usuarios.findIndex(u =>u.email === email);
+      if(email_existente !== -1){
+      return res.status(409).json({mensagem: "email já está cadastrado"})
+      }
+      usuario.email = email;
+      usuario.nome = nome;
+      // if (nome) usuario.nome = nome;
+      // if (email) usuario.email = email; #tem algum erro lógico aq!!!!!!!!!!
+    }
+
+    res.json({mensagem: `usuario ${usuario.nome}, do id ${usuario.id} foi alterado`});
+
+
+}
+function deletarUsuario(req,res){
+    const { id } = req.params;
+    const idnumber = parseInt(id);
+    if(isNaN(idnumber)){
+      return res.status(400).json({mensagem: "o id precisa ser um número inteiro"})
+    }
+    let usuario_deletado = usuarios.findIndex(u => u.id == idnumber);
+    let usuario_deletado_info = usuarios.find(u => u.id == idnumber);
+
+    if(usuario_deletado === -1){
+        return res.status(404).json({mensagem: "usuario não foi encontrado!"});
+    };
+
+
+    
+    res.json({mensagem: `usuario ${usuario_deletado_info.nome}, do id ${usuario_deletado_info.id} foi deletado`});
+    usuarios.splice(usuario_deletado, 1)
+
+}
+export {ListarUsuarios, CriarUsuario, pegar1Usuario, atualizarUsuario, deletarUsuario}
--- routes/users-router.js
+++ routes/users-router.js
@@ -1,122 +1,27 @@
 import express from "express";
 import Router from "express";
-import { ListarUsuarios } from "../controller/user-controller";
+import { ListarUsuarios, CriarUsuario, pegar1Usuario, atualizarUsuario } from "../controller/user-controller.js";
 
 const roteador = Router();
 
 // passar as rotas para ele 
 roteador.get('/', (req, res) =>{
-    // const limit = parseInt(req.query.limit) || usuarios.length;
-    // res.json({mensagem: "foi?"})
-    // res.status(200).json(usuarios.slice(0, limit));
-    ListarUsuarios();
+
+    ListarUsuarios(req,res);
 
 });
 roteador.post('/', (req, res) =>{
-
-
-    const {nome, email} = req.body;
-    if(!nome || !email){
-        return res.status(400).json({mensagem: "nome e email obrigatórios"});
-    }
-
-    const email_valido = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-
-    if(!email_valido.test(email)){
-        return res.status(400).json({mensagem: "email inválido!!"})
-    }
-
-    ultimoId++;
-    const novoUsuario = { id: ultimoId, nome, email };
-    usuarios.push(novoUsuario);
-    return res.status(201).json({mensagem: "criado"});
-
-    
+    CriarUsuario(req,res);
 });
 roteador.get('/:id', (req, res) =>{
-    const { id } = req.params;
-    const usuario = usuarios.find(u => u.id == idnumber)
-    const idnumber = parseInt(id);
-    if(isNaN(idnumber)){
-      return res.status(400).json({mensagem: "o id precisa ser um número inteiro"})
-    }
-    if(!usuario){
-        return res.status(404).json({mensagem: "usuario não encontrado!!"});
-    }
-    res.json(usuario);
+    pegar1Usuario(req,res);
 });
 roteador.patch('/:id', (req, res) =>{
-    const { id } = req.params;
-    const idnumber = parseInt(id);
-    const { nome, email} = req.body;
-    
-
-    const usuario = usuarios.find(u =>u.id == idnumber);
-    if(isNaN(idnumber)){
-      return res.status(400).json({mensagem: "o id precisa ser um número inteiro"})
-    }
- 
-    if(!usuario){
-        return res.status(404).json({mensagem: "usuario não encontrado!!!"});
-    };
-
-
-
-
-    if(email){
-      let email_existente = usuarios.findIndex(u =>u.email === email);
-      if(email_existente !== -1){
-      return res.status(409).json({mensagem: "email já está cadastrado"})
-      }
-      usuario.email = email
-    }
-
-    if(!nome && !email){
-      return res.status(400).json({mensagem: "manda pelo menos um"})
-    }
-
-    if(nome){
-      let nome_existente = usuarios.findIndex(u =>u.nome === nome);
-      if(nome_existente !== -1){
-      return res.status(409).json({mensagem: "email já está cadastrado"})
-      }
-      usuario.nome = nome
-    }
-
-    if(email && nome){
-      let email_existente = usuarios.findIndex(u =>u.email === email);
-      if(email_existente !== -1){
-      return res.status(409).json({mensagem: "email já está cadastrado"})
-      }
-      usuario.email = email;
-      usuario.nome = nome;
-      // if (nome) usuario.nome = nome;
-      // if (email) usuario.email = email; #tem algum erro lógico aq!!!!!!!!!!
-    }
-
-    res.json({mensagem: `usuario ${usuario.nome}, do id ${usuario.id} foi alterado`});
-
-
+    atualizarUsuario(req,res);
 });
 
 roteador.delete('/:id', (req, res) =>{
-    const { id } = req.params;
-    const idnumber = parseInt(id);
-    if(isNaN(idnumber)){
-      return res.status(400).json({mensagem: "o id precisa ser um número inteiro"})
-    }
-    let usuario_deletado = usuarios.findIndex(u => u.id == idnumber);
-    let usuario_deletado_info = usuarios.find(u => u.id == idnumber);
-
-    if(usuario_deletado === -1){
-        return res.status(404).json({mensagem: "usuario não foi encontrado!"});
-    };
-
-
-    
-    res.json({mensagem: `usuario ${usuario_deletado_info.nome}, do id ${usuario_deletado_info.id} foi deletado`});
-    usuarios.splice(usuario_deletado, 1)
-
+    deletarUsuario(req,res);
 });
 
 export default roteador;
